d8802243ef65b8084db9b2cbaf14031e4ecbef14
Thakane <122876128+N-Eiman@users.noreply.github.com>
2024-06-29 20:22:13 +0200

    first commit

---
+++ index.py
@@ -0,0 +1,31 @@
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+#Creating a figure and 3D Axes
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+#Generating data for plotting
+x = np.linspace(-5,5,100)
+y = np.linspace(-5,5,100)
+
+x,y =np.meshgrid(x,y)
+
+z = np.sin(np.sqrt(x**2 + y**2))
+
+#Plotting the surface
+
+ax.plot_surface(x,y,z, color ='pink', cmap='viridis')
+
+#Set labels
+
+ax.set_xlabel('x axis')
+ax.set_ylabel('y axis')
+ax.set_zlabel('z axis')
+
+#display the plot
+
+plt.show()
+
